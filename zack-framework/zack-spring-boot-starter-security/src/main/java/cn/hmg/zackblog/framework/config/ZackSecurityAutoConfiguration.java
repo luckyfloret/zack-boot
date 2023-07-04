9e1919d8e505999041d273490059a27903533693
hmg <2663069776@qq.com>
2023-07-04 23:22:39 +0800

    新增security自动配置类

---
+++ zack-framework/zack-spring-boot-starter-security/src/main/java/cn/hmg/zackblog/framework/config/ZackSecurityAutoConfiguration.java
@@ -0,0 +1,14 @@
+package cn.hmg.zackblog.framework.config;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+
+/**
+ * @author hmg
+ * @version 1.0
+ * @date 2023-07-04 23:18
+ * @description: security自动配置类
+ */
+@AutoConfiguration
+public class ZackSecurityAutoConfiguration {
+
+}
