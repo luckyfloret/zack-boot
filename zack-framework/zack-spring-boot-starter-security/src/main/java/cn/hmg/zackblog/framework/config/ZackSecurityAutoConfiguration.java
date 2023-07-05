3fd39a6910a805afeabc6ea02a85eaeb8814c96d
hmg <2663069776@qq.com>
2023-07-05 23:24:56 +0800

    新增spring security自动配置类、spring security属性配置类、还有一些依赖

--- zack-framework/zack-spring-boot-starter-security/src/main/java/cn/hmg/zackblog/framework/config/ZackSecurityAutoConfiguration.java
+++ zack-framework/zack-spring-boot-starter-security/src/main/java/cn/hmg/zackblog/framework/config/ZackSecurityAutoConfiguration.java
@@ -1,6 +1,8 @@
 package cn.hmg.zackblog.framework.config;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 /**
  * @author hmg
@@ -9,6 +11,7 @@ import org.springframework.boot.autoconfigure.AutoConfiguration;
  * @description: security自动配置类
  */
 @AutoConfiguration
+@EnableConfigurationProperties(value = SecurityProperties.class)
 public class ZackSecurityAutoConfiguration {
 
 }
