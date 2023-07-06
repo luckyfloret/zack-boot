e065b83b78d1c942735e17593e2de5bed5395025
hmg <2663069776@qq.com>
2023-07-06 11:44:42 +0800

    test

--- zack-framework/zack-common/src/main/java/cn/hmg/zackblog/common/pojo/CommonResult.java
+++ zack-framework/zack-common/src/main/java/cn/hmg/zackblog/common/pojo/CommonResult.java
@@ -1,13 +1,24 @@
 package cn.hmg.zackblog.common.pojo;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
 import java.io.Serializable;
+
 /**
  * @author hmg
  * @version 1.0
  * @date 2023-07-06 10:18
  * @description: 封装统一返回
  */
+@Data
 @JsonInclude(value = JsonInclude.Include.NON_NULL)
 public class CommonResult<T> implements Serializable {
+    private Integer code;
+    private String message;
+    private T data;
+
+    public CommonResult(){
+
+    }
 }
