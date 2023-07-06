e06a91b5b28456e2168ec3a0f3c2f7832066eedd
luckyfloret <2663069776@qq.com>
2023-07-06 10:33:06 +0800

    test

--- zack-framework/zack-common/src/main/java/cn/hmg/zackblog/common/pojo/CommonResult.java
+++ zack-framework/zack-common/src/main/java/cn/hmg/zackblog/common/pojo/CommonResult.java
@@ -1,10 +1,14 @@
 package cn.hmg.zackblog.common.pojo;
 
+import lombok.Data;
+
 /**
  * @author hmg
  * @version 1.0
  * @date 2023-07-06 10:18
  * @description: 封装统一返回
  */
+@Data
 public class CommonResult {
+
 }
