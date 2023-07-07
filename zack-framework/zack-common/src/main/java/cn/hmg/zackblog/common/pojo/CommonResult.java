113ddd97108375139e729e02f099631072c8e902
hmg <2663069776@qq.com>
2023-07-07 23:29:53 +0800

    完善统一返回结果类，新增全局异常枚举类，新增服务器异常、业务异常类

--- zack-framework/zack-common/src/main/java/cn/hmg/zackblog/common/pojo/CommonResult.java
+++ zack-framework/zack-common/src/main/java/cn/hmg/zackblog/common/pojo/CommonResult.java
@@ -1,5 +1,6 @@
 package cn.hmg.zackblog.common.pojo;
 
+import cn.hmg.zackblog.common.exception.enums.GlobalErrorCode;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
@@ -18,7 +19,38 @@ public class CommonResult<T> implements Serializable {
     private String message;
     private T data;
 
-    public CommonResult(){
-
+    public CommonResult(Integer code, String message){
+        this.code = code;
+        this.message = message;
     }
+
+    public CommonResult(Integer code, String message, T data){
+        this(code, message);
+        this.data = data;
+    }
+
+    public static <T> CommonResult<T> success(){
+        return new CommonResult<>(200, "success");
+    }
+
+    public static <T> CommonResult<T> success(Integer code, String message, T data){
+        return new CommonResult<>(code, message, data);
+    }
+
+    public static <T> CommonResult<T> success(T data){
+        return new CommonResult<>(200, "success", data);
+    }
+
+    public static <T> CommonResult<T> error(){
+        return new CommonResult<>(GlobalErrorCode.SERVER_ERROR.getCode(), GlobalErrorCode.SERVER_ERROR.getMessage());
+    }
+
+    public static <T> CommonResult<T> error(Integer code, String message, T data){
+        return new CommonResult<>(code, message, data);
+    }
+
+    public static <T> CommonResult<T> error(Integer code, String message){
+        return new CommonResult<>(code, message);
+    }
+
 }
