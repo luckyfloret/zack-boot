818c7edf0c38d7dc31afac86957e63e99f1864e6
hmg <2663069776@qq.com>
2023-07-08 14:20:16 +0800

    优化目录结构

--- zack-module-system/src/main/java/cn/hmg/zackblog/module/system/controller/admin/permission/MenuController.java
+++ zack-module-system/src/main/java/cn/hmg/zackblog/module/system/controller/admin/permission/MenuController.java
@@ -1,18 +1,18 @@
-package cn.hmg.zackblog.module.system.controller.dict;
+package cn.hmg.zackblog.module.system.controller.admin.permission;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * <p>
- * 字典类型 前端控制器
+ * 后台系统菜单 前端控制器
  * </p>
  *
  * @author hmg
  * @since 2023-07-02
  */
 @Controller
-@RequestMapping("/module.system/dictType")
-public class DictTypeController {
+@RequestMapping("/module.system/menu")
+public class MenuController {
 
 }
