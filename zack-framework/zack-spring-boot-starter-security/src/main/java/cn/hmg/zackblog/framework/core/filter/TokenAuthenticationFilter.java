ad7e4292d7754cb606a32f8831b3ea034b7fd431
hmg <2663069776@qq.com>
2023-07-08 23:36:05 +0800

    新增授权失败、认证失败处理类，新增token过滤器和一些配置与maven依赖

---
+++ zack-framework/zack-spring-boot-starter-security/src/main/java/cn/hmg/zackblog/framework/core/filter/TokenAuthenticationFilter.java
@@ -0,0 +1,23 @@
+package cn.hmg.zackblog.framework.core.filter;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author hmg
+ * @version 1.0
+ * @date 2023-07-08 16:39
+ * @description: token认证过滤器
+ */
+public class TokenAuthenticationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        filterChain.doFilter(request, response);
+    }
+}
