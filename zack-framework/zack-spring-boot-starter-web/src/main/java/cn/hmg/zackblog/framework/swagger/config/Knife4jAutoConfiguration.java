7d03ed0e9c87bcd5d48b677ab9cbab5d7751c294
hmg <2663069776@qq.com>
2023-07-13 23:51:26 +0800

    把openApi2更新为openApi3

--- zack-framework/zack-spring-boot-starter-web/src/main/java/cn/hmg/zackblog/framework/swagger/config/Knife4jAutoConfiguration.java
+++ zack-framework/zack-spring-boot-starter-web/src/main/java/cn/hmg/zackblog/framework/swagger/config/Knife4jAutoConfiguration.java
@@ -1,15 +1,22 @@
 package cn.hmg.zackblog.framework.swagger.config;
 
+import cn.hutool.core.util.RandomUtil;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.oas.models.parameters.HeaderParameter;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.customizers.GlobalOpenApiCustomizer;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+import org.springframework.http.HttpHeaders;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author hmg
@@ -18,26 +25,55 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
  * @description:
  */
 @AutoConfiguration
-@EnableSwagger2WebMvc
 public class Knife4jAutoConfiguration {
 
-    @Bean(value = "defaultApi2")
-    public Docket defaultApi2() {
-        Docket docket=new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(new ApiInfoBuilder()
-                        //.title("swagger-bootstrap-ui-demo RESTful APIs")
-                        .description("ZackBlog RESTful APIs")
-                        .termsOfServiceUrl("https://www.zack6.cn")
-                        .contact(new Contact("hmg", "https://www.zack6.cn", "2663069776@qq.com"))
+    /**
+     * 根据@Tag 上的排序，写入x-order
+     *
+     * @return the global open api customizer
+     */
+    @Bean
+    public GlobalOpenApiCustomizer orderGlobalOpenApiCustomizer() {
+        return openApi -> {
+            if (openApi.getTags()!=null){
+                openApi.getTags().forEach(tag -> {
+                    Map<String,Object> map=new HashMap<>();
+                    map.put("x-order", RandomUtil.randomInt(0,100));
+                    tag.setExtensions(map);
+                });
+            }
+            if(openApi.getPaths()!=null){
+                openApi.addExtension("x-test123","333");
+                openApi.getPaths().addExtension("x-abb",RandomUtil.randomInt(1,100));
+            }
+
+        };
+    }
+
+    @Bean
+    public GroupedOpenApi userApi(){
+        String[] paths = { "/**" };
+        String[] packagedToMatch = { "cn.hmg.zackblog.module.system.controller" };
+        return GroupedOpenApi.builder().group("用户模块")
+                .pathsToMatch(paths)
+                .addOperationCustomizer((operation, handlerMethod) -> {
+                    return operation.addParametersItem(new HeaderParameter().name("groupCode").example("测试").description("集团code").schema(new StringSchema()._default("BR").name("groupCode").description("集团code")));
+                })
+                .packagesToScan(packagedToMatch).build();
+    }
+    @Bean
+    public OpenAPI customOpenApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("zack-blog系统API")
                         .version("1.0")
-                        .build())
-                //分组名称
-                .groupName("2.X版本")
-                .select()
-                //这里指定Controller扫描包路径
-                .apis(RequestHandlerSelectors.basePackage("cn.hmg.zackblog.module.system.controller"))
-                .paths(PathSelectors.any())
-                .build();
-        return docket;
+
+                        .description( "Knife4j集成springdoc-openapi示例")
+                        .termsOfService("http://doc.xiaominfo.com")
+                        .license(new License().name("Apache 2.0")
+                                .url("http://doc.xiaominfo.com"))
+                ).addSecurityItem(new SecurityRequirement().addList(HttpHeaders.AUTHORIZATION))
+                .components(new Components().addSecuritySchemes(HttpHeaders.AUTHORIZATION,new SecurityScheme()
+                        .name(HttpHeaders.AUTHORIZATION).type(SecurityScheme.Type.HTTP).scheme("bearer")));
     }
 }
