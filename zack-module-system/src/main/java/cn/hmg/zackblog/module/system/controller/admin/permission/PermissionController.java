4be2b78bdcfbffb2cc9a015cc9e3c09fb39b4d28
hmg <2663069776@qq.com>
2023-07-18 23:19:31 +0800

    新增AdminAuthMenuRespVO，AdminAuthPermissionRespVO,PermissionController,PermissionService,PermissionServiceImpl

---
+++ zack-module-system/src/main/java/cn/hmg/zackblog/module/system/controller/admin/permission/PermissionController.java
@@ -0,0 +1,18 @@
+package cn.hmg.zackblog.module.system.controller.admin.permission;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author hmg
+ * @version 1.0
+ * @date 2023-07-18 16:29
+ * @description: 权限控制器
+ */
+@Tag(name = "权限控制")
+@RestController
+@RequestMapping("/admin/system/permission")
+public class PermissionController {
+
+}
---
+++ zack-module-system/src/main/java/cn/hmg/zackblog/module/system/service/permission/PermissionService.java
@@ -0,0 +1,11 @@
+package cn.hmg.zackblog.module.system.service.permission;
+
+/**
+ * @author hmg
+ * @version 1.0
+ * @date 2023-07-18 16:28
+ * @description: 权限服务
+ */
+public interface PermissionService {
+
+}
