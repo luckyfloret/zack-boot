c27e80d9fedc84df27f1f30a5090bb8680086b1b
hmg <2663069776@qq.com>
2023-07-25 23:57:27 +0800

    完善BaseMapperExtend接口

--- zack-framework/zack-spring-boot-starter-mybatis-plus/src/main/java/cn/hmg/zackblog/framework/core/mapper/BaseMapperExtend.java
+++ zack-framework/zack-spring-boot-starter-mybatis-plus/src/main/java/cn/hmg/zackblog/framework/core/mapper/BaseMapperExtend.java
@@ -1,9 +1,14 @@
 package cn.hmg.zackblog.framework.core.mapper;
 
+import cn.hmg.zackblog.common.pojo.PageQueryParam;
+import cn.hmg.zackblog.common.pojo.PageResult;
 import cn.hmg.zackblog.framework.core.query.LambdaQueryWrapperExtend;
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.core.toolkit.support.SFunction;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 
 import java.util.List;
 
@@ -14,21 +19,29 @@ import java.util.List;
  * @description: BaseMapper的扩展接口
  */
 public interface BaseMapperExtend<T> extends BaseMapper<T> {
+
+    default PageResult<T> page(PageQueryParam pageQueryParam, Wrapper<T> queryWrapper) {
+        IPage<T> page = selectPage(new Page<>(pageQueryParam.getPageNum(), pageQueryParam.getPageSize()), queryWrapper);
+
+        return new PageResult<>(page.getRecords(), page.getTotal());
+    }
+
     /**
      * 查询一个
+     *
      * @param field 字段名
      * @param value 值
      * @return T
      */
-    default T selectOne(SFunction<T, ?> field, Object value){
+    default T selectOne(SFunction<T, ?> field, Object value) {
         return selectOne(new LambdaQueryWrapperExtend<T>().eq(field, value));
     }
 
-    default T selectOne(SFunction<T, ?> field1, Object value1, SFunction<T, ?> field2, Object value2){
+    default T selectOne(SFunction<T, ?> field1, Object value1, SFunction<T, ?> field2, Object value2) {
         return selectOne(new LambdaQueryWrapperExtend<T>().eq(field1, value1).eq(field2, value2));
     }
 
-    default T selectOne(SFunction<T, ?> field1, Object value1, SFunction<T, ?> field2, Object value2,  SFunction<T, ?> field3, Object value3){
+    default T selectOne(SFunction<T, ?> field1, Object value1, SFunction<T, ?> field2, Object value2, SFunction<T, ?> field3, Object value3) {
         return selectOne(new LambdaQueryWrapperExtend<T>().eq(field1, value1).eq(field2, value2).eq(field3, value3));
     }
 
@@ -36,7 +49,11 @@ public interface BaseMapperExtend<T> extends BaseMapper<T> {
         return selectCount(new LambdaQueryWrapperExtend<T>().eq(field1, value1));
     }
 
-    default List<T> selectList(){
+    default List<T> selectList() {
         return selectList(new QueryWrapper<>());
+    }
+
+    default void delete(SFunction<T, ?> field1, Object value1) {
+        delete(new LambdaQueryWrapperExtend<T>().eq(field1, value1));
     }
 }
