bb095799ebd1e5ad1bcb35873f3789f595efd03b
hmg <2663069776@qq.com>
2023-07-31 19:24:19 +0800

    新增Spring Boot Admin配置文件，新增Spring Boot Admin依赖

---
+++ zack-module-infra/src/main/java/cn/hmg/zackblog/module/infra/config/AdminServerConfiguration.java
@@ -0,0 +1,16 @@
+package cn.hmg.zackblog.module.infra.config;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author hmg
+ * @version 1.0
+ * @date 2023-07-30 9:52
+ * @description:
+ */
+@Configuration
+@EnableAdminServer
+public class AdminServerConfiguration {
+
+}
