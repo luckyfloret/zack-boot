61b68c053d4a537e65bb578a07864288931c5039
hmg <2663069776@qq.com>
2023-08-24 00:02:09 +0800

    新增一些增强方法

--- zack-framework/zack-spring-boot-starter-mybatis-plus/src/main/java/cn/hmg/zackblog/framework/mybatisplus/core/query/LambdaQueryWrapperExtend.java
+++ zack-framework/zack-spring-boot-starter-mybatis-plus/src/main/java/cn/hmg/zackblog/framework/mybatisplus/core/query/LambdaQueryWrapperExtend.java
@@ -13,16 +13,49 @@ import java.util.Objects;
  * @description: LambdaQueryWrapper的扩展类
  */
 public class LambdaQueryWrapperExtend<T> extends LambdaQueryWrapper<T> {
-    public LambdaQueryWrapperExtend<T> likeIfExists(SFunction<T, ?> column, String value){
+    public LambdaQueryWrapperExtend<T> likeIfExists(SFunction<T, ?> column, String value) {
         if (StrUtil.isNotEmpty(value)) {
             return (LambdaQueryWrapperExtend<T>) super.like(column, value);
         }
         return this;
     }
-        public LambdaQueryWrapperExtend<T> eqIfExists(SFunction<T, ?> column, Object value){
+
+    public LambdaQueryWrapperExtend<T> eqIfExists(SFunction<T, ?> column, Object value) {
         if (Objects.nonNull(value)) {
             return (LambdaQueryWrapperExtend<T>) super.eq(column, value);
         }
+        return this;
+    }
+
+    public LambdaQueryWrapperExtend<T> gtIfExists(SFunction<T, ?> column, Object value) {
+        if (Objects.nonNull(value)) {
+            return (LambdaQueryWrapperExtend<T>) super.gt(column, value);
+        }
+
+        return this;
+    }
+
+    public LambdaQueryWrapperExtend<T> geIfExists(SFunction<T, ?> column, Object value) {
+        if (Objects.nonNull(value)) {
+            return (LambdaQueryWrapperExtend<T>) super.ge(column, value);
+        }
+
+        return this;
+    }
+
+    public LambdaQueryWrapperExtend<T> ltIfExists(SFunction<T, ?> column, Object value) {
+        if (Objects.nonNull(value)) {
+            return (LambdaQueryWrapperExtend<T>) super.lt(column, value);
+        }
+
+        return this;
+    }
+
+    public LambdaQueryWrapperExtend<T> leIfExists(SFunction<T, ?> column, Object value) {
+        if (Objects.nonNull(value)) {
+            return (LambdaQueryWrapperExtend<T>) super.le(column, value);
+        }
+
         return this;
     }
 }
