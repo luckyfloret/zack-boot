5d82955c1935c0b4c5a10adcd93e1b66e727ba25
hmg <2663069776@qq.com>
2023-10-05 22:27:13 +0800

    新增条件配置，根据条件启用mq组件

--- zack-framework/zack-spring-boot-starter-mq/src/main/java/cn/hmg/zackblog/framework/rocketmq/autoconfigure/ZackRocketMQAutoConfiguration.java
+++ zack-framework/zack-spring-boot-starter-mq/src/main/java/cn/hmg/zackblog/framework/rocketmq/autoconfigure/ZackRocketMQAutoConfiguration.java
@@ -3,6 +3,8 @@ package cn.hmg.zackblog.framework.rocketmq.autoconfigure;
 import cn.hmg.zackblog.framework.rocketmq.core.RocketMQTemplateExt;
 import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 
 /**
@@ -12,8 +14,10 @@ import org.springframework.context.annotation.Bean;
  * @description: rocketmq自动配置类
  */
 @AutoConfiguration
+@ConditionalOnProperty(matchIfMissing = true, prefix = "zack.rocketmq", value = "enabled", havingValue = "true")
 public class ZackRocketMQAutoConfiguration {
 
+    @ConditionalOnMissingBean
     @Bean
     public RocketMQTemplateExt rocketMQTemplateExt(RocketMQTemplate rocketMQTemplate){
         return new RocketMQTemplateExt(rocketMQTemplate);
